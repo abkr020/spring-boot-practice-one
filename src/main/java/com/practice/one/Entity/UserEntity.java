7ddfac022ff1fc95c0e3a75eed0497e86cf8c3d4
abkr020 <abkr.020@gmail.com>
2025-06-17 23:01:00 +0530

    tabel structure is created in postgres

---
+++ src/main/java/com/practice/one/Entity/UserEntity.java
@@ -0,0 +1,18 @@
+package com.practice.one.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable=false,unique=true)
+    private String email;
+    @Column(nullable = false)
+    private String pass;
+}
